5bef148813b5a30c367ab76a50237eae435d7f7a
Pk11 <epicpkmn11@outlook.com>
2020-06-27 07:36:15 -0500

    Make dbsuggest use an embed

--- commands/Univ-Team/dbsuggest.js
+++ commands/Univ-Team/dbsuggest.js
@@ -1,10 +1,15 @@
 module.exports = {
-    name: 'dbsuggest',
-    usage: '',
-    desc: 'Suggest an application for Universal DB',
-    DM: true,
-    permissions: [],
-    exec(UnivBot, msg) {
-        return msg.send('Follow the template specified here: https://github.com/Universal-Team/db/issues/new?assignees=&labels=app+request&template=app-request.md');
-    }
-};
+	name: 'dbsuggest',
+	usage: '',
+	desc: 'Suggest an application for Universal DB',
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		return msg.send({"embed": {
+			"title": "Universal DB app request",
+			"url": "https://github.com/Universal-Team/db/issues/new?assignees=&labels=app+request&template=app-request.md",
+			"description": "You can suggest apps by submitting an Issue following this template on the GitHub repository.",
+			"color": 51330
+		}});
+	}
+}

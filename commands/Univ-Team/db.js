5de2fe54666accb66f9647461068b31d56341dfd
Pk11 <epicpkmn11@outlook.com>
2020-06-27 12:32:46 -0500

    Add send all mode for DB search

--- commands/Univ-Team/db.js
+++ commands/Univ-Team/db.js
@@ -1,37 +1,40 @@
 module.exports = {
 	name: ['DB', 'UniversalDB'],
-	usage: '<app|description>',
+	usage: '<app|description> [--all]',
 	desc: 'Search for an application on Universal DB',
 	DM: true,
 	permissions: [],
 	exec(UnivBot, msg) {
 		if (msg.args.length == 0)
 			return msg.send('**Error:** Please enter a search term');
+		
 
-		let query = msg.args.trim().toLowerCase();
-
+		let query = msg.args.trim().toLowerCase().split(" ");
+		let all = query.includes("--all");
+		query = query.filter(r => !r.startsWith("--")).join(" ");
 		
 		require('node-fetch')("https://db.universal-team.net/data/full.json", {"method": "Get"}).then(r => r.json()).then(json => {
-			let res;
+			let out = [];
 			// Search titles
 			json.some(app => {
 				if(app.title && app.title.toLowerCase().includes(query)) {
-					res = app;
-					return true;
+					out.push(app);
+					return !all;
 				}
 			});
 
-			if(!res) {
+			if(out.length == 0 || all) {
 				// Search descriptions
 				json.some(app => {
 					if(app.description && app.description.toLowerCase().includes(query)) {
-						res = app;
-						return true;
+						if(!out.includes(app))
+							out.push(app);
+						return !all;
 					}
 				});
 			}
 
-			if(res) {
+			for(let res of out) {
 				let embed = {"embed": {
 					"title": res.title,
 					"url": "https://db.universal-team.net/" + res.system.toLowerCase() + "/" + Array.from(res.title.toLowerCase().replace(/ /g, "-")).filter(r => "abcdefghijklmnopqrstuvwxyz0123456789-_.".includes(r)).join(""),
@@ -55,7 +58,9 @@ module.exports = {
 				}
 
 				msg.send(embed);
-			} else {
+			}
+
+			if(out.length == 0) {
 				msg.send("No results found!");
 			}
 		});

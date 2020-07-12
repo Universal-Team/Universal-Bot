2b8c918fb3ead108120d2f8b3e24e1b1da96673b
NightScript <maorninja@outlook.com>
2020-07-12 12:02:47 -0700

    Update strings & allow messages older than two weeks to be deleted

--- commands/Moderation/purge.js
+++ commands/Moderation/purge.js
@@ -4,24 +4,19 @@ module.exports = {
 	desc: 'Deletes a certain amount of messages',
 	permissions: [ 'MANAGE_MESSAGES' ],
 	async exec(UnivBot, msg) {
-		// Setup vars
-		var err1 = 'Can\'t delete messages older than 2 weeks';
-		var err2 = 'Can\'t delete more than 100 messages at a time';
-		var err3 = 'The provided number isn\'t valid';
-
 		// Check the number
-		var number = parseInt(msg.args);
+		const number = parseInt(msg.args);
 		if (number.toString().includes('-') || number.toString().includes('.') || number.toString() == '0' || !number.toString().length || number.toString() == 'NaN'  || number.toString().includes('e') || number.toString() == 'Infinity')
-			return msg.send(err3);
+			return msg.send("The provided number is not valid.");
 		if (number > 100)
-			return msg.send(err2);
+			return msg.send("Only 100 messages can be deleted at a time.");
 
 		// Delete messages
 		await msg.delete();
 		try {
-			await msg.channel.bulkDelete(number);
+			await msg.channel.bulkDelete(number, true);
 		} catch(e) {
-			msg.send(err1)
+			msg.send("There was an error when attempting to bulk delete the messages.");
 		}
 	}
 }

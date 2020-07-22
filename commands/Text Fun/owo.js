b09a6eff6819a7c66a401119f335331a96fe94ad
XDgierman <34095690+XDgierman@users.noreply.github.com>
2020-07-22 21:46:06 +0200

    slightish addon of rules
    might think out something else, i dunno

--- commands/Text Fun/owo.js
+++ commands/Text Fun/owo.js
@@ -14,11 +14,14 @@ module.exports = {
 
 function OwOify(text) {
 	return 'OwO ' + text
-		.replace('speak', 'spweak')
 		.replace('need', 'nweed')
+		.replace('speak', 'spweak')
 		.replace('stand', 'stwand')
+		.replace('time', 'tim')
+		.replace('worse', 'wose')
 		.replace(/[rl]/gm, "w")
 		.replace(/[RL]/gm, "W")
 		.replace(/ove/g, 'uv')
+		.replace(/\b(ha|hah|heh|hehe)+\b/g, 'hehe xD')
 		.trim() + ' OwO';
 }

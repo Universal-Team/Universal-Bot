01dc3a92defe4f490937524405ed3a6a0a7cff07
Pk11 <epicpkmn11@outlook.com>
2020-07-22 18:18:01 -0500

    Update ?owo a bit

--- commands/Text Fun/owo.js
+++ commands/Text Fun/owo.js
@@ -1,35 +1,36 @@
+const faces = ['OwO', 'UwU', '>w<', '^w^', '-w-', 'XwX', 'TwT'];
+
 module.exports = {
-	name: ['owo', 'owoify'],
-	usage: '<Text>',
-	desc: 'Makes a message look like OWO',
+	name: ['owo', 'owoify', 'uwu', 'uwuify'],
+	usage: '<Text> [--last] [-l]',
+	desc: 'OwO-ify a message',
 	DM: true,
 	permissions: [],
 	exec(UnivBot, msg) {
 		if (!msg.args.length)
-			return msg.send('No owo fow you');
+			return msg.send('No ' + faces[Math.floor(Math.random() * faces.length)] + ' fow you');
+		else if(msg.args == '-l' || msg.args == '--last')
+			msg.args = Array.from(msg.channel.messages.cache)[msg.channel.messages.cache.size - 2][1].content;
 
 		msg.send('**' + msg.author.tag + '**: ' + OwOify(msg.args));
 	}
 }
 
 function OwOify(text) {
-	var random_number = Math.round(Math.random());
-	var owo_text = '';
-	if (random_number == 1){
-		owo_text += 'OwO';
-		} else {
-			owo_text += 'UwU'
-		}
-	return owo_text + ' ' + text
-		.replace('need', 'nweed')
-		.replace('oh no', 'ono')
-		.replace('speak', 'spweak')
-		.replace('stand', 'stwand')
-		.replace('time', 'tim')
-		.replace('worse', 'wose')
+	return text
+		.replace(/need/g, 'nweed')
+		.replace(/oh no/g, 'ono')
+		.replace(/speak/g, 'spweak')
+		.replace(/stand/g, 'stwand')
+		.replace(/time/, 'tim')
+		.replace(/worse/, 'wose')
 		.replace(/[rl]/gm, "w")
 		.replace(/[RL]/gm, "W")
 		.replace(/ove/g, 'uv')
+		.replace(/na/g, 'nya')
+		.replace(/that/g, 'thawt')
+		.replace(/this/g, 'thiws')
+
 		.replace(/\b(ha|hah|heh|hehe)+\b/g, 'hehe xD')
-		.trim() + ' ' + owo_text;
+		.trim() + ' ' + faces[Math.floor(Math.random() * faces.length)];
 }

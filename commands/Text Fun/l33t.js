3f5914222f74ef55e95372fd9551cc2ed981451c
XDgierman <34095690+XDgierman@users.noreply.github.com>
2020-07-30 16:56:52 +0200

    Update l33t.js (#5)

--- commands/Text Fun/l33t.js
+++ commands/Text Fun/l33t.js
@@ -16,19 +16,20 @@ module.exports = {
 }
 
 function l33t(text){
-  return text
-    .replace(/hacker/gi, 'h4x0r')
+	return text
+		.replace(/hacker/gi, 'h4x0r')
 		.replace(/hack|hacks/gi, 'h4x')
-    .replace(/Sorry|sorry/gi, 'sry')
-    .replace(/The|the/gi, 't3h')
-    .replace(/and/gi, 'nd')
+		.replace(/Sorry|sorry/gi, 'sry')
+		.replace(/and/gi, 'nd')
 		.replace(/Dude|dude/gi, 'd00d')
 		.replace(/Wait|wait/gi, 'w8')
 		.replace(/Easy|easy/gi, 'ez pz')
-    .replace(/[Aa]/g, '4')
-    .replace(/[Ee]/g, '3')
-    .replace(/[Oo]/g, '0')
-    .replace(/[Ii]/g, '1')
-    .replace(/[FU]/g, 'V')
-    .replace(/[fu]/g, 'v')
+		.replace(/[Aa]/g, '4')
+		.replace(/[Ee]/g, '3')
+		.replace(/[Oo]/g, '0')
+		.replace(/[LlIi]/g, '1')
+		.replace(/[Tt]/g, '7')
+		.replace(/7h3/gi, 't3h')
+		.replace(/[FU]/g, 'V')
+		.replace(/[fu]/g, 'v')
 }

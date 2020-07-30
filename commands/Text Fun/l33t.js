bf7dad1d3e72156847add12c2e22170556140493
XDgierman <34095690+XDgierman@users.noreply.github.com>
2020-07-30 13:31:23 +0200

    l33t sp34ch c0nv3rt3r (#4)
    * l33t sp34ch c0nv3rt3r
    
    * Update l33t.js
    
    * Update l33t.js

---
+++ commands/Text Fun/l33t.js
@@ -0,0 +1,34 @@
+module.exports = {
+	name: ['leet', 'l33t', '1337', 'h4x0r'],
+	usage: '[--last|-l] [message]',
+	desc: 'l33t pr0gr4m1ng sp33ch',
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		if(msg.args == '-l' || msg.args == '--last')
+			msg.args = msg.channel.messages.cache.array()[msg.channel.messages.cache.size - 2].content;
+
+		if(!msg.args.length)
+			return msg.send('git gut scrub');
+
+		msg.send(l33t(msg.args));
+	}
+}
+
+function l33t(text){
+  return text
+    .replace(/hacker/gi, 'h4x0r')
+		.replace(/hack|hacks/gi, 'h4x')
+    .replace(/Sorry|sorry/gi, 'sry')
+    .replace(/The|the/gi, 't3h')
+    .replace(/and/gi, 'nd')
+		.replace(/Dude|dude/gi, 'd00d')
+		.replace(/Wait|wait/gi, 'w8')
+		.replace(/Easy|easy/gi, 'ez pz')
+    .replace(/[Aa]/g, '4')
+    .replace(/[Ee]/g, '3')
+    .replace(/[Oo]/g, '0')
+    .replace(/[Ii]/g, '1')
+    .replace(/[FU]/g, 'V')
+    .replace(/[fu]/g, 'v')
+}

3ec3cc4697b8c7afb1c9521154ff095048e5e439
Pk11 <epicpkmn11@outlook.com>
2020-10-13 12:19:30 -0500

    Add ?fancyFont

---
+++ commands/Text Fun/fancyFont.js
@@ -0,0 +1,24 @@
+const valid = [0, 2, 4, 7, 8, 9, 10, 11, 12];
+
+module.exports = {
+	name: [ 'fancyFont', 'fancyText'],
+	usage: '[-__l__ast] [--__i__ndex ' + valid.join(", ") + '] [message]',
+	desc: 'Uses mathematical letters to make text look like a different font',
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		if(msg.args.last || msg.args.l)
+			msg.args.value = msg.channel.messages.cache.array()[msg.channel.messages.cache.size - 2].content;
+
+		if(!msg.args.value)
+			return msg.send('How am I supposed to make nothing look fancy?'.fancyFont(valid[Math.floor(Math.random() * valid.length)]));
+			
+		let index = parseInt(msg.args.i ?? msg.args.index ?? valid[Math.floor(Math.random() * valid.length)]);
+		console.log(index)
+
+		if(!valid.includes(index))
+			return msg.send(('Index must be ' + valid.join(", ") + '!').fancyFont(valid[Math.floor(Math.random() * valid.length)]));
+
+		msg.send(msg.args.value.fancyFont(index));
+	}
+}
--- utils/Prototypes.js
+++ utils/Prototypes.js
@@ -1,3 +1,8 @@
 String.prototype.madeOf = function(charset) {
 	return this.split('').filter(letter => charset.includes(letter)).length == this.length;
 }
+
+String.prototype.fancyFont = function(index) {
+	return this.replace(/[A-Z]/g, r => String.fromCodePoint(r.codePointAt(0) + 0x1D3BF + (52 * index)))
+			   .replace(/[a-z]/g, r => String.fromCodePoint(r.codePointAt(0) + 0x1D3B9 + (52 * index)));
+}

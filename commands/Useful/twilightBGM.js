b75759ae87f42630013700f39a746a1b46ba29d9
Pk11 <epicpkmn11@outlook.com>
2021-03-31 05:06:34 -0500

    Edit message for downloading, converting, sending

--- commands/Useful/twilightBGM.js
+++ commands/Useful/twilightBGM.js
@@ -9,7 +9,7 @@ module.exports = {
 	desc: "Converts audio to TWiLight Menu++'s BGM format",
 	DM: true,
 	permissions: [],
-	exec(UnivBot, msg) {
+	async exec(UnivBot, msg) {
 		let link = msg.args.value;
 		if(msg.attachments.first())
 			link = msg.attachments.first().attachment;
@@ -24,6 +24,8 @@ module.exports = {
 		if(!name)
 			return msg.send("Invalid file name!");
 
+		let m = await msg.send("Downloading... (This may take a bit)");
+
 		fetch(link, {"method": "Get"}).then(r => {
 			if(r.status >= 200 && r.status <= 299) {
 				return r.buffer();
@@ -31,13 +33,14 @@ module.exports = {
 				throw Error(response.statusText);
 			}
 		}).then(r => {
-			fs.writeFileSync(name, r)
-			msg.send("Converting... (This may take a bit)");
+			m.edit("Converting... (This may take a bit)");
+			fs.writeFileSync(name, r);
 			spawnSync("ffmpeg", ["-y", "-i", name, "-f", "s16le", "-acodec", "pcm_s16le", "-ac", "1", "-ar", "16k", "bgm.pcm.raw"], { encoding: "utf-8" });
 			if(fs.existsSync(name)) {
 				fs.unlinkSync(name);
 			}
-			msg.send("", MessageAttachment(fs.readFileSync("bgm.pcm.raw"), "bgm.pcm.raw"));
+			m.edit("Sending... (This may take a bit)");
+			msg.send("", MessageAttachment(fs.readFileSync("bgm.pcm.raw"), "bgm.pcm.raw")).then(() => m.edit("Done!"));
 			if(fs.existsSync("bgm.pcm.raw")) {
 				fs.unlinkSync("bgm.pcm.raw");
 			}

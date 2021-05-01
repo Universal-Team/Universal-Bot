afc1b0e1e454ed66bfac6bfce750519a043e9a94
Pk11 <epicpkmn11@outlook.com>
2021-05-01 05:49:46 -0500

    Add -r to ?emoji

--- commands/Text Fun/emoji.js
+++ commands/Text Fun/emoji.js
@@ -1,14 +1,25 @@
 module.exports = {
 	name: ["emoji", "絵文字"],
-	usage: "<emoji names>",
+	usage: "[-__r__andom] <emoji names>",
 	desc: "Sends any Discord emoji from a guild Universal-Bot is in",
 	DM: true,
 	permissions: [],
 	exec(UnivBot, msg) {
-		if(!msg.args.value)
+		if(!(msg.args.value || msg.args.random || msg.args.r))
 			return msg.send("Nothing isn't an emoji...");
 
-		let out = msg.args.value.replace(/[^\s]+/g, r => {
+		let out = "";
+		if(msg.args.random || msg.args.r) {
+			let guild = UnivBot.client.guilds.cache.array();
+			guild = guild[Math.floor(Math.random() * guild.length)];
+
+			let emoji = guild.emojis.cache.array();
+			emoji = emoji[Math.floor(Math.random() * emoji.length)];
+
+			out = `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> `;
+		}
+
+		out += msg.args.value.replace(/[^\s]+/g, r => {
 			let emoji;
 			UnivBot.client.guilds.cache.some(guild => {
 				return guild.emojis.cache.some(e => {

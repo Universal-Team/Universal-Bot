a1c4bcfc48932a5cb5b5cbc702fe3b4e54e6dfaf
Pk11 <epicpkmn11@outlook.com>
2021-06-14 09:11:29 -0500

    Add missing spaces

--- commands/Univ-Team/db.js
+++ commands/Univ-Team/db.js
@@ -105,13 +105,13 @@ module.exports = {
 					res.description += `\n[Also on ${res.systems[i]}](https://db.universal-team.net/${res.systems[i].toLowerCase()}/${Array.from(res.title.toLowerCase().replace(/ /g, "-")).filter(r => "abcdefghijklmnopqrstuvwxyz0123456789-_.".includes(r)).join("")})`;
 
 				if(res.version) {
-					res.description += `\n**Version**:${res.version}`;
+					res.description += `\n**Version**: ${res.version}`;
 					if(res.version_title)
 						res.description += `, ${res.version_title}`;
 				}
 
 				if(res.license)
-					res.description += `\n**License**:${res.license_name}`;
+					res.description += `\n**License**: ${res.license_name}`;
 
 				let embed = {"embed": {
 					"title": res.title,

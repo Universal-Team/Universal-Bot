8fa27b2dffeeba04e06e03fe1890ce772a66cecd
Pk11 <epicpkmn11@outlook.com>
2021-08-16 16:28:30 -0500

    Add --count option to ?gimmeWord

--- commands/Text Fun/gimmeWord.js
+++ commands/Text Fun/gimmeWord.js
@@ -1,13 +1,23 @@
 module.exports = {
 	name: ["gimmeWord", "randomWord"],
-	usage: "",
+	usage: "[--__c__ount]",
 	desc: "Gives a random word",
 	DM: true,
 	permissions: [],
 	exec(UnivBot, msg) {
-		let names = require("../../data/unicode-names.json");
-		out = names[Object.keys(names)[Math.floor(Math.random() * Object.keys(names).length)]].split(/\b/).filter(r => r.match(/\b/));
 
-		msg.reply(out[Math.floor(Math.random() * out.length)].toLowerCase());
+		let count = parseInt(msg.args.count || msg.args.c);
+		if(isNaN(count) || count < 1)
+			count = 1;
+
+		let out = [];
+
+		const names = require("../../data/unicode-names.json");
+		for(let i = 0; i < count; i++) {
+			let str = names[Object.keys(names)[Math.floor(Math.random() * Object.keys(names).length)]].split(/\b/).filter(r => r.match(/\b/));
+			out.push(str[Math.floor(Math.random() * str.length)].toLowerCase());
+		}
+
+		msg.reply(out.join(" "));
 	}
 }

e0c5f444079d92e0ad7afdff6694fcbb8a3f23f6
Pk11 <epicpkmn11@outlook.com>
2021-08-23 15:27:42 -0500

    Add ?mooh

---
+++ commands/Text Fun/mooh.js
@@ -0,0 +1,18 @@
+const {CommandInteraction} = require("discord.js");
+
+module.exports = {
+	name: ["mooh", "muuh", "moo"],
+	usage: "[-__l__ast] [message]",
+	desc: "Mooh-ify a message",
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		if(msg.args.last || msg.args.l)
+			msg.args.value = msg.channel.messages.cache.map(r => r)[msg.channel.messages.cache.size - (msg instanceof CommandInteraction ? 1 : 2)].content;
+
+		if(!msg.args.value)
+			return msg.reply("Moooooooooooooh");
+
+		msg.reply(msg.args.value.replace(/\b\w*?[aiueoy]/g, msg.cmd));
+	}
+}

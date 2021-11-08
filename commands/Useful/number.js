1f9a61697e160f6e104db2676cac393867c249e3
Pk11 <epicpkmn11@outlook.com>
2021-11-08 15:49:24 -0600

    Add ?number

---
+++ commands/Useful/number.js
@@ -0,0 +1,61 @@
+module.exports = {
+	name: [ "number", "hex", "decimal", "octal", "binary" ],
+	usage: "[-__r__andom] [number]",
+	desc: "Shows a number in various bases",
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		let base = 10;
+		if(msg.args.value[0] === "0") {
+			if(msg.args.value[1].toLowerCase() == "x") {
+				base = 16;
+				msg.args.value = msg.args.value.substr(2);
+			} else if(msg.args.value[1].toLowerCase() == "b") {
+				base = 2;
+				msg.args.value = msg.args.value.substr(2);
+			} else if(msg.args.value[1].toLowerCase() == "o") {
+				base = 8;
+				msg.args.value = msg.args.value.substr(2);
+			} else {
+				base = 8;
+				msg.args.value = msg.args.value.substr(1);
+			}
+		}
+		let number = parseInt(msg.args.value, base);
+		console.log(number);
+
+		if(msg.args.random || msg.args.r)
+			number = Math.floor(Math.random() * 0x10000);
+
+		if(isNaN(number))
+			return msg.reply("What am I supposed to do with something that isn't even a number...");
+
+		return msg.reply({
+			embeds: [{
+				title: `Number: ${number}`,
+				fields: [
+					{
+						"name": "Hexadecimal (Base 16)",
+						"value": `0x${number.toString(16).toUpperCase()}`,
+						"inline": true
+					},
+					{
+						"name": "Decimal (Base 10)",
+						"value": number.toString(10),
+						"inline": true
+					},
+					{
+						"name": "Octal (Base 8)",
+						"value": number == 0 ? "0" : `0${number.toString(8)}`,
+						"inline": true
+					},
+					{
+						"name": "Binary (Base 2)",
+						"value": `0b${number.toString(2)}`,
+						"inline": true
+					}
+				]
+			}]
+		});
+	}
+}

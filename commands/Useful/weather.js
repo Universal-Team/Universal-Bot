ea18a7405be60d0f638e893cc6217cf5183ea87e
Pk11 <epicpkmn11@outlook.com>
2021-11-15 02:57:33 -0600

    Set color based on skycode

--- commands/Useful/weather.js
+++ commands/Useful/weather.js
@@ -1,5 +1,56 @@
 const weather = require("weather-js");
 
+const colors = [
+	0x00008B, // Thunderstorm
+	0x00008B, // Thunderstorm
+	0x00008B, // Thunderstorm
+	0x00008B, // Thunderstorm
+	0x00008B, // Thunderstorm
+	0xADD8E6, // Rain/Snow mix
+	0xADD8E6, // Sleet/Snow mix
+	0xADD8E6, // Rain/Sleet/Snow mix
+	0xADD8E6, // Icy
+	0xADD8E6, // Icy
+	0x4169E1, // Rain/Sleet mix
+	0x4169E1, // Light Rain
+	0x4169E1, // Rain
+	0xFFFFFF, // Light Snow
+	0xFFFFFF, // Snow
+	0xFFFFFF, // Blizzard
+	0xFFFFFF, // Snow
+	0x00008B, // Thunderstorm
+	0x4169E1, // Showers
+	0xF5F5DC, // Dust
+	0xA9A9A9, // Fog
+	0xA9A9A9, // Haze
+	0x808080, // Smoke
+	0xD3D3D3, // Windy
+	0xD3D3D3, // Windy
+	0xADD8E6, // Frigid
+	0xD3D3D3, // Cloudy
+	0xD3D3D3, // Partly Cloudy (night)
+	0xD3D3D3, // Partly Cloudy
+	0xD3D3D3, // Partly Cloudy (night)
+	0xD3D3D3, // Partly Cloudy
+	0x202020, // Clear (night)
+	0x87CEEB, // Clear
+	0xD3D3D3, // Partly Cloudy (night)
+	0xD3D3D3, // Partly Cloudy
+	0x00008B, // Thunderstorm
+	0xFFA500, // Hot
+	0x00008B, // Scattered Thunderstorms
+	0x00008B, // Scattered Thunderstorms
+	0x4169E1, // Scattered Showers
+	0x4169E1, // Showers
+	0xADD8E6, // Scattered Snow Showers
+	0xFFFFFF, // Snow
+	0xFFFFFF, // Snow
+	0xFF00FF, // N/A
+	0x4169E1, // Scattered Rain Showers (night)
+	0xFFFFFF, // Scattered Snow Showers (night)
+	0x00008B  // Scattered Thunderstorms (night)
+];
+
 module.exports = {
 	name: ["weather", "temperature", "temp"],
 	usage: "[-__f__ahrenheit] <location>",
@@ -24,7 +75,7 @@ module.exports = {
 			let data = result[0];
 
 			msg.reply({embeds: [{
-				color: 0x87CEEB,
+				color: colors[data.current.skycode],
 				title: `Weather in ${data.location.name}`,
 				thumbnail: {
 					url: data.current.imageUrl

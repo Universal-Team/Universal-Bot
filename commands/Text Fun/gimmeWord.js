6a36f0ca907141f504a2be69cab4808a9921ad53
Pk11 <epicpkmn11@outlook.com>
2021-11-16 23:54:21 -0600

    Add waiting message to ?gimmeword

--- commands/Text Fun/gimmeWord.js
+++ commands/Text Fun/gimmeWord.js
@@ -12,7 +12,7 @@ module.exports = {
 	desc: "Gives a random word",
 	DM: true,
 	permissions: [],
-	exec(UnivBot, msg) {
+	async exec(UnivBot, msg) {
 		let count = parseInt(msg.args.count || msg.args.c);
 		if(isNaN(count))
 			count = 1;
@@ -23,12 +23,16 @@ module.exports = {
 
 		let out = [];
 
+		let reply = await msg.reply("working on it...");
+
 		const names = require("../../data/unicode-names.json");
 		for(let i = 0; i < count; i++) {
 			let str = names[Object.keys(names)[Math.floor(Math.random() * Object.keys(names).length)]].split(/\b/).filter(r => r.match(/\b/));
 			out.push(str[Math.floor(Math.random() * str.length)].toLowerCase());
+			if(i == 50)
+				await reply.edit("why's this json so big...");
 		}
 
-		msg.reply(out.join(" ").substr(0, 2000));
+		reply.edit(out.join(" ").substr(0, 2000));
 	}
 }

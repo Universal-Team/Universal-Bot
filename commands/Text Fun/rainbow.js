6098306911cd9a40c437f5bb791c91a53e6da296
Pk11 <epicpkmn11@outlook.com>
2022-02-07 15:13:10 -0600

    Add ?rainbow

---
+++ commands/Text Fun/rainbow.js
@@ -0,0 +1,26 @@
+const {CommandInteraction, Formatters} = require("discord.js");
+
+module.exports = {
+	name: ["rainbow", "rainbowify"],
+	args: {
+		last: {
+			letter: "l"
+		},
+		value: {
+			title: "message"
+		}
+	},
+	desc: "Makes the message a rainbow",
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		if(msg.args.last || msg.args.l)
+			msg.args.value = msg.channel.messages.cache.map(r => r)[msg.channel.messages.cache.size - (msg instanceof CommandInteraction ? 1 : 2)].content;
+
+		if(!msg.args.value)
+			return msg.reply("I can't rainbowify the void");
+
+		let i = 0;
+		msg.reply(Formatters.codeBlock("ansi", msg.args.value.replace(/[^\s]/g, match => `\x1B[0;${31 + (i++ % 6)}m${match}`)));
+	}
+}

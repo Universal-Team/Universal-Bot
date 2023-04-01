17826ef4ab2cfa483e47ae143a46821f320cf6ca
Pk11 <pk11@outlook.jp>
2023-04-01 17:49:09 -0500

    Add ?StackZ

---
+++ commands/Fun/stackz.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "StackZ",
+	args: {},
+	desc: "Lets you know what StackZ has to say about the current situation",
+	DM: true,
+	permissions: [],
+	exec(UnivBot, msg) {
+		msg.reply("https://i.imgur.com/kQUG6pq.png");
+	}
+}

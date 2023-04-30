08ee0ea6a148003d620572d8cda614ef5f2898a6
Pk11 <pk11@outlook.jp>
2023-04-30 08:59:54 -0500

    Fix ?dbsuggest

--- commands/Univ-Team/dbsuggest.js
+++ commands/Univ-Team/dbsuggest.js
@@ -7,7 +7,7 @@ module.exports = {
 	exec(UnivBot, msg) {
 		return msg.reply({embeds: [{
 			"title": "Universal DB app request",
-			"url": "https://github.com/Universal-Team/db/issues/new?assignees=&labels=app+request&template=app-request.md",
+			"url": "https://github.com/Universal-Team/db/issues/new?assignees=&labels=app+request&template=app-request.yml",
 			"description": "You can suggest apps by submitting an Issue following this template on the GitHub repository.",
 			"color": 51330
 		}]});
